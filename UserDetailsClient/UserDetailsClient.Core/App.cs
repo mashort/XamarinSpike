450a881c4b81e10be32b3792da9bc97b77fed85e
martins <martin.short@qnrl.com>
2018-08-22 09:31:53 +0100

    Add initial attempt at ADB2C login

---
+++ UserDetailsClient/UserDetailsClient.Core/App.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Identity.Client;
+
+using Xamarin.Forms;
+
+namespace UserDetailsClient.Core
+{
+    public class App : Application
+    {
+        public static PublicClientApplication PCA = null;
+
+        // Azure AD B2C Coordinates
+        public static string Tenant = "<Teneant name here>";
+        public static string ClientID = "<Application ID here>";
+        public static string PolicySignUpSignIn = "<Policy SignUp SignIn here>";
+        public static string[] Scopes = { "" }; // { "https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read" };
+        public static string ApiEndpoint = "<Api endpooint here>";
+
+        public static string AuthorityBase = $"https://login.microsoftonline.com/tfp/{Tenant}/";
+        public static string Authority = $"{AuthorityBase}{PolicySignUpSignIn}";
+        public static UIParent UiParent = null;
+
+        public App()
+        {
+            // default redirectURI; each platform specific project will have to override it with its own
+            PCA = new PublicClientApplication(ClientID, Authority);
+            PCA.RedirectUri = $"msal{ClientID}://auth";
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ UserDetailsClient/UserDetailsClient.Droid/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Microsoft.Identity.Client;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using UserDetailsClient.Core;
+using Xamarin.Forms.Platform.Android;
+
+namespace UserDetailsClient.Droid
+{
+    [Activity(Label = "UserDetailsClient", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+            App.UiParent = new UIParent(this);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
+        }
+    }
+}
+
